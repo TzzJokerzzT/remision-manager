2c79deed42e8a8b531e8dcc61e53c9e8aee667ef
Alexis Buelvas <alexjesus-4856@hotmail.com>
2026-09-21 21:55:13 -0500

    chore(refactor): change env.ts config file

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -1,4 +1,4 @@
 export const appConfig = {
-  apiUrl: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3002/api',
+  apiUrl: process.env.CONFIG_API_URL ?? 'http://localhost:3002/api',
   // apiUrl: 'http://localhost:3002/api',
 } as const;
